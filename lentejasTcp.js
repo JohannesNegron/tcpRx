b64f126093e23bff345c2092e7e22ab477f9823f
Juan Jose Negron Granados <juseng7@gmail.com>
2018-11-09 16:30:54 -0600

    datos

--- lentejasTcp.js
+++ lentejasTcp.js
@@ -3,9 +3,10 @@ var pg 	=	require('pg');
 
 var PORT_SERVER=6969;
 
-var conString = "postgres://postgres:NorjEpWy@localhost:5432/lentejasbd";
+//var conString = "postgres://postgres:NorjEpWy@localhost:5432/lentejasbd";
 //var objSensors={"device":1,"sensors":{"agua_temp":20.56,"ph":35.31,"lux":1240},"timestamp":"17/07/25,11:05:28-20"};
 
+/*
 var client = new pg.Client(conString);
 client.connect(function(err) 
 {
@@ -18,12 +19,15 @@ client.connect(function(err)
 		console.log('connection succed!');
 	}	
 });
+*/
 
 net.createServer(function(socket)
 {
 	console.log('CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
 	socket.on('data', function(data) 
 	{
+		console.log(save_data(data));
+		/*
         console.log('DATA ' + socket.remoteAddress + ': ' + data);
 		//socket.write('You said "' + data + '"');
 		socket.write("1");
@@ -41,6 +45,7 @@ net.createServer(function(socket)
 				return console.error('error running query', err);
 			}
 		});
+		*/
 	});
 
     // Add a 'close' event handler to this instance of socketet
@@ -52,3 +57,30 @@ net.createServer(function(socket)
 {
 	console.log('Server listening on port' + PORT_SERVER);
 });
+function save_data(data)
+{
+	pg.connect("postgres://postgres:NorjEpWy@localhost:5432/lentejasbd", (err, client, done)=>
+	{
+		if(err)
+		{
+			return console.error("Error al conectarse", err);
+		}
+		var sentence_query = "INSERT INTO sensors (ph, temp, light, date, device) VALUES ("+
+			data['sensors']['ph']+", "+
+			data['sensors']['agua_temp']+", "+
+			data['sensors']['lux']+", TIMESTAMP '"+
+			data['timestamp']+"', "+
+			data["device"]+")";
+		
+		client.query(sentence_query, function(err, result)
+		{
+			done();
+			if(err) 
+			{
+				return console.error('error running query', err);
+			}
+			console.log(result)
+			return 1;
+		});
+	});
+};
