7c0ba805c9929e3c645534dc9f4f9c2ca1bdab17
Juan Jose Negron Granados <juseng7@gmail.com>
2018-11-12 15:56:33 -0500

    ps a ver

--- lentejasTcp.js
+++ lentejasTcp.js
@@ -3,54 +3,21 @@ var pg 	=	require('pg');
 
 var PORT_SERVER=6969;
 
-//var conString = "postgres://postgres:NorjEpWy@localhost:5432/lentejasbd";
 //var objSensors={"device":1,"sensors":{"agua_temp":20.56,"ph":35.31,"lux":1240},"timestamp":"17/07/25,11:05:28-20"};
-
-/*
-var client = new pg.Client(conString);
-client.connect(function(err) 
-{
-	if(err) 
-	{
-		return console.error('could not connect to postgres', err);
-	}
-	else
-	{
-		console.log('connection succed!');
-	}	
-});
-*/
 
 net.createServer(function(socket)
 {
 	console.log('CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
 	socket.on('data', function(data) 
 	{
-<<<<<<< HEAD
-	        console.log('DATA ' + socket.remoteAddress + ': ' + data);
-=======
-		console.log(save_data(data));
-		/*
-        console.log('DATA ' + socket.remoteAddress + ': ' + data);
->>>>>>> b64f126093e23bff345c2092e7e22ab477f9823f
-		//socket.write('You said "' + data + '"');
-		console.log("enviando 1");
-		socket.write("1");
-		data=JSON.parse(data);
-		var sentence_query = "INSERT INTO sensors (ph, temp, light, date, device) VALUES ("+
-		data['sensors']['ph']+", "+
-		data['sensors']['agua_temp']+", "+
-		data['sensors']['lux']+", TIMESTAMP '"+
-		data['timestamp']+"', "+
-		data["device"]+")";
-		client.query(sentence_query, function(err, result)
+		save_data(JSON.parse(data))
+		.then((value)=>
 		{
-			if(err) 
+			if(value)
 			{
-				return console.error('error running query', err);
+				socket.write("1");
 			}
 		});
-		*/
 	});
 
     // Add a 'close' event handler to this instance of socketet
@@ -62,33 +29,37 @@ net.createServer(function(socket)
 {
 	console.log('Server listening on port' + PORT_SERVER);
 });
-<<<<<<< HEAD
-=======
 function save_data(data)
 {
-	pg.connect("postgres://postgres:NorjEpWy@localhost:5432/lentejasbd", (err, client, done)=>
+	console.log(data);
+	return new Promise((resolve, reject)=>
 	{
-		if(err)
+		pg.connect("postgres://postgres:NorjEpWy@localhost:5432/lentejasbd", (err, client, done)=>
 		{
-			return console.error("Error al conectarse", err);
-		}
-		var sentence_query = "INSERT INTO sensors (ph, temp, light, date, device) VALUES ("+
-			data['sensors']['ph']+", "+
-			data['sensors']['agua_temp']+", "+
-			data['sensors']['lux']+", TIMESTAMP '"+
-			data['timestamp']+"', "+
-			data["device"]+")";
-		
-		client.query(sentence_query, function(err, result)
-		{
-			done();
-			if(err) 
+			if(err)
 			{
-				return console.error('error running query', err);
+				console.log("Error al conectarse", err);
+				console.log(err);
+				resolve(0);
 			}
-			console.log(result)
-			return 1;
+			var sentence_query = "INSERT INTO sensors (ph, temp, light, date, device) VALUES ("+
+				data['sensors']['ph']+", "+
+				data['sensors']['agua_temp']+", "+
+				data['sensors']['lux']+", TIMESTAMP '"+
+				data['timestamp']+"', "+
+				data["device"]+")";
+			
+			client.query(sentence_query, function(err, result)
+			{
+				done();
+				if(err) 
+				{
+					console.log('error running query', err);
+					resolve(0);
+				}
+				console.log(result)
+				resolve(1);
+			});
 		});
-	});
+	})
 };
->>>>>>> b64f126093e23bff345c2092e7e22ab477f9823f
